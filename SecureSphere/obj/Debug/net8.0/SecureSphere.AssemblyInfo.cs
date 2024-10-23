a23d7ecba357ec391c5f248ce76fe2012b3aced7
Hamad Maghaireh <electronics.hamad@gmail.com>
2024-10-23 18:00:39 +0300

    new

--- SecureSphere/obj/Debug/net8.0/SecureSphere.AssemblyInfo.cs
+++ SecureSphere/obj/Debug/net8.0/SecureSphere.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecureSphere")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea747bcd9a3da5a1404a0a9bb9d0d49016b7149a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d833d5a09e56009789ff56683df337fda8934dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecureSphere")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecureSphere")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
